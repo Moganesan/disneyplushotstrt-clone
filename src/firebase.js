81f7dd8bb402897eae6388bf59ae730f19f3bd1f
Moganesan <43741383+Moganesan@users.noreply.github.com>
2022-03-19 17:44:39 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB1b1BFDPpyrPN8QuHsuLP6lPtIED8wKVM",
-  authDomain: "disney-plus-clone-56f96.firebaseapp.com",
-  projectId: "disney-plus-clone-56f96",
-  storageBucket: "disney-plus-clone-56f96.appspot.com",
-  messagingSenderId: "364061015120",
-  appId: "1:364061015120:web:046e963049ddb428dfe555",
+  apiKey: "************************************",
+  authDomain: "*****************************",
+  projectId: "************************88",
+  storageBucket: "8********************************8",
+  messagingSenderId: "88888888888",
+  appId: "1:364061015120:web:8******8888888888888888",
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
